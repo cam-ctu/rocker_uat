4beb8b4ba38489df1915c46718871a6747c7d623
shug0131 <sjb277@medschl.cam.ac.uk>
2024-11-21 16:48:44 +0000

    pass mostly nearly there

--- edit_log.R
+++ edit_log.R
@@ -1,7 +1,29 @@
 edit_log <- function(name="dummy",result=1,date_stamp=as.character(Sys.time())){
   load("log.Rdata")
-  log <- rbind(log,c(name, result, date_stamp))
+  old <- log
+  # It needs to be able to overwrite previous entries by matching name
+  index <- log$name!=name
+  log <- log[index,]
+  
+  log <- rbind(log,
+               data.frame(name=name, result=result, date_stamp=date_stamp)
+               )
+  # This keeps the correct variable names
   save(log, file="log.Rdata")
-  # This doesn;t keep the correct variable names
-  # It needs to be able to overwrite previous entires by matching name
+  invisible(old)
 }
+
+clear_log <- function(){
+  load("log.Rdata")
+  old <- log
+  log <- data.frame(name=character(0),result=integer(0),date_stamp=character(0))
+  save(log, file="log.Rdata")
+  invisible(old)
+}
+
+
+# edit_log("mew",0)
+# load("log.Rdata")
+# log
+# df <- clear_log()
+# df
